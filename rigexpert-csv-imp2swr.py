e75fa86613754b69fb59de2590552366a9f96012
Vanya A. Sergeev <v@sergeev.io>
2016-10-05 01:16:02 -0700

    csv-imp2swr: use argparse and simplify arguments

--- rigexpert-csv-imp2swr.py
+++ rigexpert-csv-imp2swr.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 import math
 import scipy.signal
@@ -5,6 +6,10 @@ import scipy.signal
 def read_csv(f):
     for line in f:
         yield tuple(line.split(","))
+
+def write_csv(f, stream):
+    for fields in stream:
+        f.write(",".join(["{}".format(e) for e in fields]) + "\n")
 
 def imp_to_vswr(stream):
     for (freq, r, x) in stream:
@@ -31,36 +36,36 @@ def smooth_vswr(stream, num_taps=64, alpha=0.06):
 
     return zip(freqs, filt_vswrs)
 
-def dump_vswr(stream):
-    for (freq, vswr) in stream:
-        print("{},{}".format(freq, vswr))
-
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Convert sweep CSV from impedance to VSWR.")
-        print("")
-        print("Usage: {} [--smooth] <impedance sweep CSV>".format(sys.argv[0]))
-        print("")
-        print("Example:")
-        print("    python3 {} sweep.imp.csv > sweep.swr.csv".format(sys.argv[0]))
-        print("")
-        print("Example (smoothed version):")
-        print("    python3 rigexpert-csv-imp2swr.py --smooth sweep.imp.csv > sweep.smooth.swr.csv")
-        print("")
-        print("Impedance CSV format:")
-        print("    <freq in MHz>,<resistance in ohms>,<reactance in ohms>")
-        print("    e.g. 15.040000,50.56,0.08 is F=15.04 MHz,R=50.56 ohms,X=0.08 ohms")
-        print("")
-        print("VSWR CSV format:")
-        print("     <freq in MHz>,<voltage swr>")
-        print("    e.g. 15.040000,1.01131434816733 is F=15.04 MHz,VSWR=1.01131434816733")
-        sys.exit(0)
+    parser = argparse.ArgumentParser(
+        description="Convert sweep CSV from impedance to VSWR.",
+        epilog=
+        "Example:\n"
+        "  {0} sweep.imp.csv sweep.swr.csv\n"
+        "\n"
+        "Example (smoothed):\n"
+        "  {0} --smooth sweep.imp.csv sweep.smooth.swr.csv\n"
+        "\n"
+        "Impedance CSV format:\n"
+        "  <freq in MHz>,<resistance in ohms>,<reactance in ohms>\n"
+        "  e.g. 15.040000,50.56,0.08 is F=15.04 MHz,R=50.56 ohms,X=0.08 ohms\n"
+        "\n"
+        "VSWR CSV format:\n"
+        "  <freq in MHz>,<voltage swr>\n"
+        "  e.g. 15.040000,1.01131434816733 is F=15.04 MHz,VSWR=1.01131434816733\n".format(sys.argv[0]),
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument("in_csv", metavar="<input CSV>", help="impedance sweep CSV")
+    parser.add_argument("out_csv", metavar="<output CSV>", help="VSWR sweep CSV")
+    parser.add_argument('--smooth', help='smooth data points', action='store_true')
+    if len(sys.argv) == 1:
+        parser.print_help()
+        parser.exit(1)
+    args = parser.parse_args()
 
-    smooth = len(sys.argv) == 3 and sys.argv[1] == "--smooth"
-    filepath = sys.argv[2] if len(sys.argv) == 3 else sys.argv[1]
-
-    with open(filepath, "r") as f:
-        if smooth:
-            dump_vswr(smooth_vswr(imp_to_vswr(read_csv(f))))
-        else:
-            dump_vswr(imp_to_vswr(read_csv(f)))
+    with open(args.in_csv, "r") as f_in:
+        with open(args.out_csv, "w") as f_out:
+            if args.smooth:
+                write_csv(f_out, smooth_vswr(imp_to_vswr(read_csv(f_in))))
+            else:
+                write_csv(f_out, imp_to_vswr(read_csv(f_in)))
